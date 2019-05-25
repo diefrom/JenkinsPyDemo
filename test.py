013ce91c88f71a76e5a7124131aefe456bfac59c
diefrom <diefrom_hdu@126.com>
2019-05-25 14:20:53 +0800

    [MOD] 添加参数读取

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 1:
         print("This is {}".format(sys.argv[1]))
     else:
         print("hello jenkins")
