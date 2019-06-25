6e2b9ffddcbb0d4927953d2f117d72223e00d78d
diefrom <diefrom_hdu@126.com>
2019-06-25 15:39:10 +0800

    [MOD] get file from jenkins

--- control_jenkins.py
+++ control_jenkins.py
@@ -1,3 +1,5 @@
+import json
+
 import jenkins
 
 class ControlJenkins(object):
@@ -10,9 +12,15 @@ class ControlJenkins(object):
     def get_version(self):
         return self.server.get_version()
 
-    def build_job(self, jobname):
-        return self.server.build_job(jobname)
+    def build_job(self, jobname, param=None):
+        return self.server.build_job(jobname, param)
 
 if __name__ == '__main__':
+    data = {"countryCode": {"value": "86", "type": "string"},
+            "email": {"value": "2508487675@qq.com", "type": "string"}}
+    strdata = json.dumps(data, separators=(',', ':'))
     control_jenkins = ControlJenkins()
-    control_jenkins.build_job("demo")
+    param = {"env": "daily", "region": "daily", "user": "2508487675@qq.com",
+             "api": "tuya.m.user.email.token.create", "v": "1.0", "data": strdata,
+             "log": "info"}
+    control_jenkins.build_job("tuya_type_check", param)
--- test.py
+++ test.py
@@ -2,5 +2,8 @@
 import os
 
 if __name__ == '__main__':
-    value = os.getenv("key")
-    print("hello {}".format(value))
+    file_path= "{}/{}".format(os.path.dirname(os.path.abspath(__file__)),
+                              "setting.json")
+    with open(file_path) as f:
+        content = f.read()
+        print(content)
